97f791724c20bdee57defa9a9abb79968f0e03da
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2025-09-15 23:02:33 +0200

    Initialized Ship class

---
+++ ship.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import Settings
+
+class Ship:
+    """A class to handle the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = Settings()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load(self.settings.ship_type)
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
