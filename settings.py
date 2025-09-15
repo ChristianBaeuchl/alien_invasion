1192fe4052ec1026ff9ed38fdf91734ae30453ac
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2025-09-15 16:22:29 +0200

    Initialized Setting class'

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize and adjust the game's settings."""
+        # Screen settings
+        self.screen_width = 1600
+        self.screen_height = 900
+        self.bg_color = (30, 130, 210) # it's blue
