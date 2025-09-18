213dc7398393c2a6df42169cfce406241500210a
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2025-09-18 22:31:50 +0200

    changed screen settings

--- settings.py
+++ settings.py
@@ -4,8 +4,10 @@ class Settings:
     def __init__(self):
         """Initialize and adjust the game's settings."""
         # Screen settings
-        self.screen_width = 1600
-        self.screen_height = 900
+        self.default_width = 1600
+        self.default_height = 900
+        self.screen_width = self.default_width
+        self.screen_height = self.default_height
         self.bg_color = (30, 130, 210) # it's blue
         # Ship settings
         self.ship_type = 'images/ship1_yellow.bmp'
