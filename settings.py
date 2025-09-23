e09c3938f592fa79c838dd07648d935f8bbfad88
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2025-09-23 18:54:55 +0200

    included bullet settings

--- settings.py
+++ settings.py
@@ -3,12 +3,21 @@ class Settings:
 
     def __init__(self):
         """Initialize and adjust the game's settings."""
+
         # Screen settings
         self.default_width = 1600
         self.default_height = 900
         self.screen_width = self.default_width
         self.screen_height = self.default_height
         self.bg_color = (30, 130, 210) # it's blue
+
         # Ship settings
         self.ship_type = 'images/ship1_yellow.bmp'
-        self.ship_speed = 2.5
+        self.ship_speed = 5
+
+        # Bullet settings
+        self.bullet_speed = 6
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (250, 60, 60)
+        self.bullets_allowed = 6
