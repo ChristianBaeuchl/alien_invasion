02afc2bfdee45362e5d82910df2ee9b0cadf4b03
ChristianBaeuchl <christian.baeuchl83@gmail.com>
2025-12-22 19:07:17 +0100

    updated (after a long time)

--- alien_invasion.py
+++ alien_invasion.py
@@ -119,11 +119,31 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
             print(len(self.bullets))
+        
+        # Check for any bullets that have hit aliens.
+        #   If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
     
     def _update_aliens(self):
-        """Update the positions of aliens in the fleet."""
+        """Check if the fleet is at an endge, then update the positions of aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
+        
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
     
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+            
+        
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
